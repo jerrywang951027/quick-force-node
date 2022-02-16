99adce06454848d64c2a1c8f0eb7381e13a4f826
Jin Wang <eternalwangjs@gmail.com>
2022-02-15 23:36:31 -0500

    1111 fix syntax error

--- app.js
+++ app.js
@@ -1,76 +1,82 @@
-var express = require('express');;
-var nforce = require('nforce');
-var hbs = require('hbs');
+var express = require("express");
+var nforce = require("nforce");
+var hbs = require("hbs");
 
 var app = express();
 
-app.set('view engine', 'hbs');
-app.enable('trust proxy');
+app.set("view engine", "hbs");
+app.enable("trust proxy");
 
 function isSetup() {
-  return (process.env.CONSUMER_KEY != null) && (process.env.CONSUMER_SECRET != null);
+  return (
+    process.env.CONSUMER_KEY != null && process.env.CONSUMER_SECRET != null
+  );
 }
 
 function oauthCallbackUrl(req) {
-  return req.protocol + '://' + req.get('host');
+  return req.protocol + "://" + req.get("host");
 }
 
-hbs.registerHelper('get', function(field) {
+hbs.registerHelper("get", function (field) {
   return this.get(field);
 });
 
-app.get('/', function(req, res) {
+app.get("/", function (req, res) {
   if (isSetup()) {
     var org = nforce.createConnection({
       clientId: process.env.CONSUMER_KEY,
       clientSecret: process.env.CONSUMER_SECRET,
       redirectUri: oauthCallbackUrl(req),
-      mode: 'single'
+      mode: "single",
     });
 
     if (req.query.code !== undefined) {
       // authenticated
-      org.authenticate(req.query, function(err) {
+      org.authenticate(req.query, function (err) {
         if (!err) {
-          org.query({ query: 'SELECT vlocity_cmt__OrchestrationPlanId__r.vlocity_cmt__OrderId__r.vlocity_cmt__FulfilmentStatus__c,Id,Name,vlocity_cmt__State__c,vlocity_cmt__OrchestrationItemDefinitionId__r.RecordType.Name,vlocity_cmt__OrchestrationItemDefinitionId__r.Name,vlocity_cmt__OrchestrationPlanDefinitionId__r.Name FROM vlocity_cmt__OrchestrationItem__c where  vlocity_cmt__State__c in ('Fatally Failed','Running')' }, function(err, results) {
-            if (!err) {
-              res.render('index', {records: results.records});
+          org.query(
+            {
+              query:
+                "SELECT vlocity_cmt__OrchestrationPlanId__r.vlocity_cmt__OrderId__r.vlocity_cmt__FulfilmentStatus__c,Id,Name,vlocity_cmt__State__c,vlocity_cmt__OrchestrationItemDefinitionId__r.RecordType.Name,vlocity_cmt__OrchestrationItemDefinitionId__r.Name,vlocity_cmt__OrchestrationPlanDefinitionId__r.Name FROM vlocity_cmt__OrchestrationItem__c where  vlocity_cmt__State__c in ('Fatally Failed','Running')",
+            },
+            function (err, results) {
+              if (!err) {
+                res.render("index", { records: results.records });
+              } else {
+                res.send(err.message);
+              }
             }
-            else {
-              res.send(err.message);
-            }
-          });
-        }
-        else {
-          if (err.message.indexOf('invalid_grant') >= 0) {
-            res.redirect('/');
-          }
-          else {
+          );
+        } else {
+          if (err.message.indexOf("invalid_grant") >= 0) {
+            res.redirect("/");
+          } else {
             res.send(err.message);
           }
         }
       });
-    }
-    else {
+    } else {
       res.redirect(org.getAuthUri());
     }
-  }
-  else {
-    res.redirect('/setup');
+  } else {
+    res.redirect("/setup");
   }
 });
 
-app.get('/setup', function(req, res) {
+app.get("/setup", function (req, res) {
   if (isSetup()) {
-    res.redirect('/');
-  }
-  else {
-    var isLocal = (req.hostname.indexOf('localhost') == 0);
+    res.redirect("/");
+  } else {
+    var isLocal = req.hostname.indexOf("localhost") == 0;
     var herokuApp = null;
-    if (req.hostname.indexOf('.herokuapp.com') > 0) {
+    if (req.hostname.indexOf(".herokuapp.com") > 0) {
       herokuApp = req.hostname.replace(".herokuapp.com", "");
     }
-    res.render('setup', { isLocal: isLocal, oauthCallbackUrl: oauthCallbackUrl(req), herokuApp: herokuApp});
+    res.render("setup", {
+      isLocal: isLocal,
+      oauthCallbackUrl: oauthCallbackUrl(req),
+      herokuApp: herokuApp,
+    });
   }
 });
 
